1f6a4304e22b9f3697856b37a37794a067e46e5c
me aku <meaku00000@gmail.com>
2022-06-04 00:59:02 +0000

    Update several lines at Hero.java file

--- Hero.java
+++ Hero.java
@@ -1,1 +1,25 @@
+public class Hero{
+    String name;
+    int age;
 
+    public Hero(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void getAge(){
+        return this.age;
+    }
+}
